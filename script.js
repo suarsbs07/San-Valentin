b9667ede1f46a70f8bfce12c48483085e06d959a
Stephano Suarez <suarezstephano07@gmail.com>
2026-02-11 01:35:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// Elements
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+const loveAudio = document.getElementById("love-audio");
+const nextLink = document.getElementById("next-link");
+
+// Click Envelope
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
+
+// Logic to move the NO btn
+
+noBtn.addEventListener("mouseover", () => {
+    const min = 200;
+    const max = 200;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Sabia que aceptarias";
+
+    catImg.src = "panda - amor.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+
+        // Mostrar botón/link
+    nextLink.style.display = "inline-block";
+
+    // Reproducir música
+    loveAudio.currentTime = 0;
+    loveAudio.play().catch(() => {
+        // Si el navegador bloquea el autoplay por alguna razón,
+        // igual ya quedó listo; normalmente con click en YES sí reproduce.
+        console.log("No se pudo reproducir automáticamente.");
+    });
+});
